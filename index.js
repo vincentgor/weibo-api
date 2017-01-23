20b98944567952eb5f59b96b0546520d5525b419
liangwensen <1149536116@qq.com>
2017-01-23 18:03:30 +0800

    新增验证码路由

--- index.js
+++ index.js
@@ -15,20 +15,24 @@ const authorize = require('./lib/oauth2/authorize');
 
 const app = koa();
 const router = Router();
+const oauth2Router = Router();
 
 app.use(bodyParser());
+
+app.use(oauth2Router.routes());
+app.use(oauth2Router.allowedMethods());
 
 /**
  * 引导用户跳转获取code
  */
-router.get('/authorize', function *(next){
+oauth2Router.get('/authorize', function *(next){
     this.redirect(authorize.getAuthorizeUrl());
 });
 
 /**
  * 根据 code 获取 access_token
  */
-router.get('/access_token', function *(next){
+oauth2Router.get('/access_token', function *(next){
     let code = this.query.code;
     let result = yield access_token(code, {});
     let ret_data = {
