34387a854967a0796a3ac56e2235543a280e20e0
liangwensen <1149536116@qq.com>
2017-01-23 17:39:56 +0800

    使用jwt进行前后端交互

--- index.js
+++ index.js
@@ -7,6 +7,8 @@
 const koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
+const JWT = require('koa-jwt');
+const jwt = require('jsonwebtoken');
 
 const app = koa();
 const router = Router();
@@ -19,24 +21,38 @@ const access_token = require('./lib/oauth2/access_token');
 const authorize = require('./lib/oauth2/authorize');
 
 /**
- * 分发路由
+ * 引导用户跳转获取code
  */
-require('./router')(router);
+router.get('/authorize', function *(next){
+    this.redirect(authorize.getAuthorizeUrl());
+});
 
 /**
  * 根据 code 获取 access_token
  */
 router.get('/access_token', function *(next){
     let code = this.query.code;
-    this.body = yield access_token(code, {});
+    let result = yield access_token(code, {});
+    let ret_data = {
+        access_token: result.body.access_token
+    };
+    this.body = {
+        result: result,
+        token: jwt.sign(ret_data, 'jerry-koala')
+    }
 });
 
 /**
- * 引导用户跳转获取code
+ * jwt验证
  */
-router.get('/authorize', function *(next){
-    this.redirect(authorize.getAuthorizeUrl());
-});
+app.use(JWT({
+    secret: 'jerry-koala'
+}));
+
+/**
+ * 分发路由(需要jwt验证)
+ */
+require('./router')(router);
 
 app.listen(3007, () => {
     console.log('微博服务已经启动，正在监听3007端口');
